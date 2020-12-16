837902e4d438c6d875a2a6e2333d27557d9b893a
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-12-16 11:01:43 -0300

    first commit

---
+++ src/test/java/com/luismarcilio/crudmvc/CrudMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismarcilio.crudmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
